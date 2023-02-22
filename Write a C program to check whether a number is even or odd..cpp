f9d7785eb254795df2371aed4746d8df3b697fdb
faiq157 <77959437+faiq157@users.noreply.github.com>
2023-02-22 09:19:22 +0500

    Add files via upload

---
+++ Write a C program to check whether a number is even or odd..cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+int number;
+cout<<"Enter Number"<<endl;
+cin>>number;
+if(number%2==0){
+	cout<<"Number is Even"<<endl;
+}
+else
+{
+	cout<<"Number is Odd";
+}
+
+	return 0;
+}
