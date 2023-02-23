9770b0661f0467b3ea71b98811945d97f6b91ccb
Faiq Ahmad <77959437+faiq157@users.noreply.github.com>
2023-02-23 17:26:35 +0500

    Add files via upload

---
+++ Write a C program to input any alphabet and check whether it is vowel or consonant..cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){	
+		char ch;
+		cout<<"Enter Charater"<<endl;
+		cin>>ch;
+		if(ch=='a' || ch=='e' || ch=='i'|| ch=='o'||ch=='u' || ch=='A' || ch=='E' || ch=='I'|| ch=='O'||ch=='U' ){
+			cout<<"Entered Chracrter is vowel..."<<endl;
+		}
+		else if(ch>='a' || ch<='z' && ch>='A' || ch<='Z')
+		{
+			cout<<" Entered Character is Consonant... ";
+		}
+
+	return 0;
+}
